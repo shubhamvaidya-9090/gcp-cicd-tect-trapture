9fe81451722079790d7b7f2ec367282a6a22ed31
Shubham Vaidya <shubham@p3clap-f54mzc2.dir.svc.accenture.com>
2024-12-03 03:46:03 -0600

    test

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def fetch_upcoming_matches():
 
     headers = {
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "1bd0a14833mshc18ed4be5953504p1236e8jsn709d3a0bc623"  # Replace with your RapidAPI key
+        "X-RapidAPI-Key": "173db70cf6msh7c7494f4639582bp1c16e1jsnb058a4789aba"  # Replace with your RapidAPI key
     }
     #
     response = requests.get(url, headers=headers)
