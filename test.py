6770bb8b4e4663de3a9a54d46332f562a3571041
Shubham Vaidya <shubham@p3clap-f54mzc2.dir.svc.accenture.com>
2024-12-03 03:45:06 -0600

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ url = "https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/international"
 
 headers = {
     "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-    "X-RapidAPI-Key": "1bd0a14833mshc18ed4be5953504p1236e8jsn709d3a0bc623"  # Replace with your RapidAPI key
+    "X-RapidAPI-Key": "173db70cf6msh7c7494f4639582bp1c16e1jsnb058a4789aba"  # Replace with your RapidAPI key
 }
 response = requests.get(url, headers=headers)
 print(response.json())
